8673306643b33a2fe73412361da18ea448a8c992
junes231 <deyongw3@gmail.com>
2025-05-27 14:59:24 +0800

    Add files via upload

--- ss-messaging-sw.js
+++ ss-messaging-sw.js
@@ -1,1 +1,5 @@
-// placeholder for SmartSender push service worker
+self['appUrl'] = 'https://customer.smartsender.eu';
+self['appToken'] = 'gK8kNmUUfxBaYA5P3pqVrj0n40rXfArZuuMcQvyk';
+self['appSenderIdentifier'] = '744213958812';
+
+self.importScripts(self['appUrl'] + '/js/client/push/fcm/worker.js');
